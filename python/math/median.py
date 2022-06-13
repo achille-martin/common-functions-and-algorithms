8a33efb9e704f71e6e8216195525dc6aa3fde529
Achille MARTIN <66834162+achille-martin@users.noreply.github.com>
2022-06-13 22:58:21 +0100

    Created median function in python

---
+++ python/math/median.py
@@ -0,0 +1,15 @@
+def median(values):
+  """Get the median of a sorted list of values
+
+  Args:
+    values (iterable of float): A list of numbers
+
+  Returns:
+    float
+  """
+  midpoint = int(len(values) / 2)
+  if len(values) % 2 == 0:
+    median = (values[midpoint - 1] + values[midpoint]) / 2
+  else:
+    median = values[midpoint]
+  return median
