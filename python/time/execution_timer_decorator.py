0c44243f3ea20c046fe6c3a63b64354e576d7ba7
Achille MARTIN <66834162+achille-martin@users.noreply.github.com>
2022-06-19 15:17:10 +0100

    Created execution timer decorator for python

---
+++ python/time/execution_timer_decorator.py
@@ -0,0 +1,15 @@
+import time
+
+def execution_timer(func):
+  def wrapper(*args, **kwargs):
+    t_start = time.time()
+    result = func(*args, **kwargs)
+    t_total = time.time() - t_start
+    print('{func_name} took {exec_time}s'.format(func_name=func.__name__, exec_time=round(t_total, 2)))
+    return result
+  return wrapper
+
+# Usage
+# @execution_timer
+# def <func_to_evaluate>(<args>):
+# # your function code
