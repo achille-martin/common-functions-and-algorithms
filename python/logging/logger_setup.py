f93d65586ae6a0da5bece5410f426901a7252bec
Achille MARTIN <66834162+achille-martin@users.noreply.github.com>
2024-03-22 23:03:25 +0000

    Improved output log folder path for practicality in logger setup script

--- python/logging/logger_setup.py
+++ python/logging/logger_setup.py
@@ -8,7 +8,10 @@ from datetime import datetime
 logger_name = 'my_script_logger'
 current_datetime = datetime.now().strftime("%Y_%m_%d-%H_%M_%S")
 logger_output_file_name = logger_name + '_' + current_datetime + '.log'
-logger_output_folder_path = os.environ.get('HOME', os.getcwd())
+logger_output_folder_path = os.path.join(
+    os.environ.get('HOME', os.getcwd(),
+    'log',
+)
 logger_output_prefix_format = "[%(asctime)s] [%(levelname)s] - %(message)s"
 logger_logging_level = "DEBUG"
 
@@ -17,7 +20,10 @@ try:
     os.makedirs(logger_output_folder_path, exist_ok=True)
     logger = log_tool.getLogger(__name__)
     logger.setLevel(logger_logging_level)
-    logger_output_file_path = os.path.join(logger_output_folder_path, logger_output_file_name)
+    logger_output_file_path = os.path.join(
+        logger_output_folder_path, 
+        logger_output_file_name,
+    )
     file_handler = log_tool.FileHandler(logger_output_file_path)
     formatter = log_tool.Formatter(logger_output_prefix_format)
     file_handler.setFormatter(formatter)
