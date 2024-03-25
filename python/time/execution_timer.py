21c8db23ede78d71e887f0081db70a598e025ea2
Achille MARTIN <66834162+achille-martin@users.noreply.github.com>
2024-03-25 14:49:22 +0000

    Fixed indentation in execution_timer

--- python/time/execution_timer.py
+++ python/time/execution_timer.py
@@ -3,19 +3,20 @@ import time
 # Context manager
 @contextlib.contextmanager
 def execution_timer():
-  """
-  Time the execution of a context block.
-  
-  Yields
-  ------
+    """
+    Time the execution of a context block.
+    
+    Yields
+    ------
     None
-  """
-  start = time.time()
-  yield
-  end = time.time()
-  print('Elapsed: ' + str(round(end - start), 2) + 's')
-
+    """
+    
+    start = time.time()
+    yield
+    end = time.time()
+    print('Elapsed: ' + str(round(end - start), 2) + 's')
+  
 # Usage
 # with execution_timer():
-#   print('This should take approximately 0.25 seconds')
-#   time.sleep(0.25)
+#     print('This should take approximately 0.25 seconds')
+#     time.sleep(0.25)
