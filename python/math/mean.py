d3a4ccf2be386a793ebde92d775d1ec3b584f926
Achille MARTIN <66834162+achille-martin@users.noreply.github.com>
2024-03-25 14:40:50 +0000

    Fixed indentation in mean

--- python/math/mean.py
+++ python/math/mean.py
@@ -1,15 +1,17 @@
 def mean(values):
-  """
-  Get the mean of a sorted list of values
-
-  Parameters
-  ----------
-  values: iterable of float
-    A list of numbers
-
-  Returns
-  -------
-    float
-  """
-  mean = sum(values) / len(values)
-  return mean
+    """
+    Get the mean of a sorted list of values
+  
+    Parameters
+    ----------
+    values: iterable of float
+      A list of numbers
+  
+    Returns
+    -------
+      float
+    """
+    
+    mean = sum(values) / len(values)
+    
+    return mean
