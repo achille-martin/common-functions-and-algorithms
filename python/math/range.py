a4fba91907e837a852e247bd89b947e97d7c3765
Achille MARTIN <66834162+achille-martin@users.noreply.github.com>
2024-03-25 15:21:09 +0000

    Created range script

---
+++ python/math/range.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+def range(values):
+    """
+    Get the range of a sorted list of values
+  
+    Parameters
+    ----------
+    values: iterable of float
+        A list of numbers
+  
+    Returns
+    -------
+    (float, float)
+        Min of values, Max of values
+    """
+    
+    minimum_value = min(values)
+    maximum_value = max(values)
+
+    return (minimum_value, maximum_value)
