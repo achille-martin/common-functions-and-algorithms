b4d83d1be0610f1f06c3117d814f2be0e980bf79
Achille MARTIN <66834162+achille-martin@users.noreply.github.com>
2024-03-25 14:53:47 +0000

    Fixed indentation in vector_search

--- cpp/vector_search.cpp
+++ cpp/vector_search.cpp
@@ -4,27 +4,27 @@
 // Inspired from https://alexsm.com/cpp-closest-lower-bound/
 
 float find_nearest(const std::vector<float>& sorted_vector, float desired_value) {
-  // Getting the index of the element which has a value >= input_value
-  auto id_nearest = std::lower_bound(sorted_vector.begin(), sorted_vector.end(), desired_value);
+    // Getting the index of the element which has a value >= input_value
+    auto id_nearest = std::lower_bound(sorted_vector.begin(), sorted_vector.end(), desired_value);
 
-  // If nearest value is at the beginning of the vector, function returns the index of the first element
-  if (id_nearest == sorted_vector.begin()) {
-      return 0;
-  }
+    // If nearest value is at the beginning of the vector, function returns the index of the first element
+    if (id_nearest == sorted_vector.begin()) {
+        return 0;
+    }
 
-  // If nearest value is at the end of the vector, function returns the index of the last element
-  if (id_nearest == sorted_vector.end()) {
-      int vector_size = sorted_vector.size();
-      return (vector_size - 1);
-  }
+    // If nearest value is at the end of the vector, function returns the index of the last element
+    if (id_nearest == sorted_vector.end()) {
+        int vector_size = sorted_vector.size();
+        return (vector_size - 1);
+    }
 
-  // If desired value is between 2 vector values, function returns the index of the closest element
-  float id_lower = *(id_nearest - 1);
-  float id_upper = *(id_nearest);
-  if (fabs(desired_value - id_lower) < fabs(desired_value - id_upper)) {
-      return id_nearest - sorted_vector.begin() - 1;
-  }
+    // If desired value is between 2 vector values, function returns the index of the closest element
+    float id_lower = *(id_nearest - 1);
+    float id_upper = *(id_nearest);
+    if (fabs(desired_value - id_lower) < fabs(desired_value - id_upper)) {
+        return id_nearest - sorted_vector.begin() - 1;
+    }
 
-  // Else, function returns the index of the corresponding element
-  return id_nearest - sorted_vector.begin();
+    // Else, function returns the index of the corresponding element
+    return id_nearest - sorted_vector.begin();
 }
