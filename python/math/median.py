ed516c7d0ac87d9087dcc07c06b687f537381063
Achille MARTIN <66834162+achille-martin@users.noreply.github.com>
2024-03-25 14:47:17 +0000

    Fixed indentation in median

--- python/math/median.py
+++ python/math/median.py
@@ -1,19 +1,23 @@
 def median(values):
-  """
-  Get the median of a sorted list of values
-
-  Parameters
-  ----------
-  values: iterable of float
-    A list of numbers
-
-  Returns
-  -------
+    """
+    Get the median of a sorted list of values
+  
+    Parameters
+    ----------
+    values: iterable of float
+        A list of numbers
+  
+    Returns
+    -------
     float
-  """
-  midpoint = int(len(values) / 2)
-  if len(values) % 2 == 0:
-    median = (values[midpoint - 1] + values[midpoint]) / 2
-  else:
-    median = values[midpoint]
-  return median
+        Median of values
+    """
+    
+    midpoint = int(len(values) / 2)
+    
+    if len(values) % 2 == 0:
+      median = (values[midpoint - 1] + values[midpoint]) / 2
+    else:
+      median = values[midpoint]
+    
+    return median
