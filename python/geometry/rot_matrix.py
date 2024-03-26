3c8ad8189b7a199c2a033351f8cb84b087b2868e
Achille MARTIN <66834162+achille-martin@users.noreply.github.com>
2024-03-26 02:09:02 +0000

    Added licence to rot_matrix and fixed import path

--- python/geometry/rot_matrix.py
+++ python/geometry/rot_matrix.py
@@ -1,8 +1,39 @@
 #!/usr/bin/env python3
 
+# MIT License
+
+# Copyright (c) 2023-2024 Achille MARTIN
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import math
 import numpy as np
-from scripts.unit_converter import unit_converter
+
+importer_folder = os.path.dirname(
+    os.path.realpath(__file__)
+)
+parent_of_importer_folder = os.path.join(
+    importer_folder,
+    os.path.pardir,
+)
+sys.path.append(parent_of_importer_folder)
+from conversions.unit_converter import unit_converter
 
 def rot_matrix(axis, angle_deg, array_type='3D'):
     """
